dc920d2a88bba9dd5a7e945e5d27a59adb5869de
ana maria <ana maria@172.30.167.178>
2017-03-06 09:25:28 -0500

    cargar core

--- src/Ejecutable.java
+++ src/Ejecutable.java
@@ -1,9 +1,19 @@
+import processing.core.PApplet;
 
-public class Ejecutable {
+public class Ejecutable extends PApplet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		PApplet.main("Ejecutable");
 
+	}
+	public void settings() {
+		
+	}
+	public void setup() {
+		
+	}
+	public void draw() {
+		
 	}
 
 }
